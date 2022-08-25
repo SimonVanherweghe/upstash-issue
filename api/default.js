0c1b57726c41abfe4e71e56b35c17f9afac45166
Simon Vanherweghe <simon.vanherweghe@gmail.com>
2022-08-25 09:53:36 +0200

    fetch todo

--- api/default.js
+++ api/default.js
@@ -1,8 +1,9 @@
 import { Redis } from "@upstash/redis"
-
+import fetch from "node-fetch"
 const redis = new Redis({ url: 'url', token: 'token' });
 
 export default async (request, response) => {
+	const todo = awit fetch('https://jsonplaceholder.typicode.com/todos/1').then(r => r.json())
 	console.log(redis);
 	response.status(200).json({
 		node_version: process.version,
