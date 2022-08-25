4183681bec91e9108cbbdd862cf992308bab72bf
Simon Vanherweghe <simon.vanherweghe@gmail.com>
2022-08-25 11:01:33 +0200

    with fetch this time...

--- api/default.js
+++ api/default.js
@@ -1,11 +1,12 @@
-import { redisTest } from "../lib/redis";
+import { redisWrite } from "../lib/redis";
 const fetch = require('@vercel/fetch')()
 
 export default async (request, response) => {
 	console.log("default")
 	const todo = await fetch('https://jsonplaceholder.typicode.com/todos/1').then(r => r.json());
 
-	console.log(redisTest);
+	await redisWrite(Date.now(), todo);
+
 	response.status(200).json({
 		node_version: process.version,
 		todo: todo,
--- api/withfetch.js
+++ api/withfetch.js
@@ -1,11 +1,12 @@
-import { redisTest } from "../lib/redis";
+import { redisWrite } from "../lib/redisWithFetch";
 const fetch = require('@vercel/fetch')()
 
 export default async (request, response) => {
-	console.log("default")
+	console.log("withfetch")
 	const todo = await fetch('https://jsonplaceholder.typicode.com/todos/1').then(r => r.json());
 
-	console.log(redisTest);
+	await redisWrite(Date.now(), todo);
+
 	response.status(200).json({
 		node_version: process.version,
 		todo: todo,
